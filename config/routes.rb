113f090f83ae3a121884967da3768c8afe66b501
tadghh <thenry.he@gmail.com>
2024-01-18 15:44:18 -0600

    integration tests exmp done

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   resources :submissions
   # Defines the root path route ("/")
   # root "posts#index"
+  root to: 'submissions#index'
 end
--- test/integration/submission_flows_test.rb
+++ test/integration/submission_flows_test.rb
@@ -1,7 +1,24 @@
-require "test_helper"
+require 'test_helper'
 
 class SubmissionFlowsTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'can see the submission index' do
+    get '/'
+    assert_select 'h1', 'Submission'
+  end
+
+  test 'can create a submission' do
+    get '/submissions/new'
+    assert_response :success
+
+    post '/submissions', {
+      params: { submission: {
+        title: 'test submission title',
+        body: 'Oh my testing content'
+      } }
+    }
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+    assert_select 'p.submission-title', "Title:\ test submission title"
+  end
 end
